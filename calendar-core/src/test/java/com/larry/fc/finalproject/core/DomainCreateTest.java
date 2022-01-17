e515a0fa301b803795d103718ad7bbcf87448199
rightnow <dldudsh83@gmail.com>
2022-01-18 07:15:51 +0900

    DomainCreateTest

--- calendar-core/src/test/java/com/larry/fc/finalproject/core/DomainCreateTest.java
+++ calendar-core/src/test/java/com/larry/fc/finalproject/core/DomainCreateTest.java
@@ -1,9 +1,8 @@
 package com.larry.fc.finalproject.core;
 
-import com.larry.fc.finalproject.core.domain.Engagement;
-import com.larry.fc.finalproject.core.domain.Event;
-import com.larry.fc.finalproject.core.domain.RequestStatus;
-import com.larry.fc.finalproject.core.domain.User;
+import com.larry.fc.finalproject.core.domain.ScheduleType;
+import com.larry.fc.finalproject.core.domain.entity.Schedule;
+import com.larry.fc.finalproject.core.domain.entity.User;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -15,16 +14,9 @@ public class DomainCreateTest {
 
     @Test
     void eventCreate(){
-        final User writer = new User("writer", "emain@emain","pw", LocalDate.now(), LocalDateTime.now());
-        final User attendee = new User("attendee", "emain@emain","pw", LocalDate.now(), LocalDateTime.now());
-        final Event event = new Event(
-                LocalDateTime.now(),
-                LocalDateTime.now(),
-                "title", "does",
-                writer,
-                LocalDateTime.now()
-        );
-        event.addEngagement(new Engagement(event, attendee,LocalDateTime.now(), RequestStatus.REQUESTED));
-        assertEquals(event.getEngagements().get(0).getEvent().getWriter().getName(),"writer");
+        final User me = new User("meme", "email", "pw", LocalDate.now() );
+        final Schedule taskSchedule = Schedule.task("할일", "청소하기", LocalDateTime.now(), me);
+        assertEquals(taskSchedule.getScheduleType(), ScheduleType.TASK);
+        assertEquals(taskSchedule.toTask().getTitle(), "할일");
     }
 }
